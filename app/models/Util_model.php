654f62f34ebb382d79b50e09c0bd6ae72905ade6
benemmylex <164938631+benemmylex@users.noreply.github.com>
2024-10-20 18:46:04 +0000

    Change smtp host to jtinvest.online

--- app/models/Util_model.php
+++ app/models/Util_model.php
@@ -365,7 +365,7 @@ class Util_model extends CI_Model
 		$this->load->library('email');
 		$config = array();
 		$config['protocol'] = 'smtp';
-		$config['smtp_host'] = 'mail.jtinvest.online';
+		$config['smtp_host'] = 'jtinvest.online';
 		$config['smtp_user'] = 'support-team@jtinvest.online';
 		$config['smtp_pass'] = 'Admin2024.';
 		$config['smtp_port'] = 587;
